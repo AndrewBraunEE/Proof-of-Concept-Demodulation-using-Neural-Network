e886adc30780475347670a953fa711136e28322e
1337eeab <andrewtb97@hotmail.com>
2019-02-20 17:34:05 -0800

    Encoder Class Buffixes

--- app/encoder/__main__.py
+++ app/encoder/__main__.py
@@ -11,13 +11,13 @@ from encoder.conversions import *
 
 class Encoder:
 	def __init__(self, **kwargs):
-		self.re_init(kwargs)
+		self.re_init(**kwargs)
 
-	def re_init(self, **kwargs)
+	def re_init(self, **kwargs):
 		try:
 			self.freq = kwargs.get('freq', 60)
 			self.encoding = kwargs.get('encoding', None)
-			self.baud = kwargs.get('baud', 2*self.frequency)
+			self.baud = kwargs.get('baud', 2*self.freq)
 			self.samples = kwargs.get('samples', 100*self.baud) # The amount of samples to use over the period of the carrier wave. (2pi)
 			self.default_mod = kwargs.get('default_mod', 'BPSK_Modulator')
 			self.BPSK_Mod = BPSK_Modulator(self.freq, self.samples, self.baud, 1)
@@ -25,6 +25,9 @@ class Encoder:
 			self.QAM_Mod =  QAM_Modulator(self.freq, self.samples, self.baud, 1)
 		except:
 			print("[ENCODER] Error in initialization of Modulators: Baudrate exceeds time-axis limit.")
+
+	def get_time_axis(input):
+		return self.BPSK_Mod.t_array[0:len(input)]
 
 	def get_raw_waveform_qam(self, input):
 		return self.QAM_Mod.modulate(input)
@@ -36,8 +39,14 @@ class Encoder:
 		return self.QPSK_Mod.modulate(input)
 
 	def get_raw_waveform_default(self, input):
-		function = getattr(self, self.default_mod)
-		return function(input)
+		function = getattr(self, 'default_mod')
+		if function == 'BPSK_Modulator':
+			return self.get_raw_waveform_bpsk(input)
+		elif function == 'QPSK_Modulator':
+			return self.get_raw_waveform_qpsk(input)
+		elif function == 'QAM_Modulator':
+			return self.get_raw_waveform_qam(input)
+		return []
 
 	def encode_default(self, input):
 		#Operations here to encode. #FIX ME
@@ -46,7 +55,7 @@ class Encoder:
 	def encode_from_str(self, input):
 		return self.encode_default(str_to_binary_str(input))
 
-	def encode_from_binary_str(self, input)
+	def encode_from_binary_str(self, input):
 		return self.encode_default(input) ##FIX ME
 
 	def save_to_file(self, input, filename): #Save an encoded message in a waveform to a file
@@ -58,18 +67,19 @@ class Encoder:
 	def load_str_dir(self, input_dir = 'data/*.txt'):
 		list_txt_files = glob.glob(input_dir)
 		data = ''
-		for file in list_txt_files
+		for file in list_txt_files:
 			with open(file, 'r') as file_resource:
-    			data=file_resource.read().replace('\n', '')
-    	return data
+				data+=file_resource.read().replace('\n', '')
+		return data
 
 def main():
 	encoder = Encoder()
 	encoder.re_init(default_mod = 'BPSK_Modulator')
 	data = encoder.encode_default(encoder.load_str_dir())
 	encoder.save_to_file(data, 'waveform_samples')
+	print(data)
 
-if __name__ == '__main__'
+if __name__ == '__main__':
 	try:
 		main()
 	except KeyboardInterrupt:
