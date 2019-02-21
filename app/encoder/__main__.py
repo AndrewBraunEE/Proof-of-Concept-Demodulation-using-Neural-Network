94ec5b45ed0b4d8720b5b975a5b7977f992806ff
1337eeab <andrewtb97@hotmail.com>
2019-02-21 13:39:02 -0800

    Numpy to TF Object Updates

--- app/encoder/__main__.py
+++ app/encoder/__main__.py
@@ -8,6 +8,7 @@ from encoder.BPSK import *
 from encoder.QPSK import *
 from encoder.QAM import *
 from encoder.conversions import *
+import tensorflow as tf
 
 #For good results, make the baud rate at least 2 times the frequency
 '''
@@ -90,24 +91,15 @@ class Encoder:
 			with open(file, 'r') as file_resource:
 				data+=file_resource.read().replace('\n', '')
 		return data
-'''
+
 	def numpy_array_to_tfdata(self, input): #Input: Numpy array in the format [(timeaxis), (waveformsamples)]^T with timeaxis on first row, waveform samples on second row with N samples for N columns
-		features = input[0]
-
-		assert features.shape[0] == labels.shape[0]
-
-		features_placeholder = tf.placeholder(features.dtype, features.shape)
-		labels_placeholder = tf.placeholder(labels.dtype, labels.shape)
-
-		dataset = tf.data.Dataset.from_tensor_slices((features_placeholder, labels_placeholder))
-		# [Other transformations on `dataset`...]
-		dataset = ...
-		iterator = dataset.make_initializable_iterator()
-
-	sess.run(iterator.initializer, feed_dict={features_placeholder: features,
-                                          labels_placeholder: labels})
-                                          '''
-def main():
+		data_tf = tf.convert_to_tensor(input, np.float64)
+		saver = tf.train.Saver(input)
+		sess = tf.InteractiveSession()  
+		saver.save(session, 'my-checkpoints', global_step = step)
+	
+                                          
+def string_to_save_tests():
 	encoder = Encoder()
 	encoder.re_init(default_mod = 'BPSK_Modulator')
 	raw_data_string = encoder.load_str_dir()
@@ -117,14 +109,24 @@ def main():
 	binary_pulse = encoder.get_modulator_default().binary_pulse(binary_str)
 	#print(binary_pulse)
 	print('[LENS]: Time: ' + str(len(time_axis)) + ' Data: ' + str(len(data)) + ' Pulse: ' + str(len(binary_pulse)) + ' String: ' + str(len(binary_str)))
-	matrix = np.row_stack((time_axis, data, binary_pulse))
+	#matrix = np.row_stack((time_axis, data))
+	matrix = numpy.array([time_axis, data, binary_pulse])
 	encoder.save_to_file(matrix, 'waveform_samples.txt')
+	print(matrix)
 	#print(data)
-	print(encoder.load_from_file('waveform_samples.txt'))
+	#print(encoder.load_from_file('waveform_samples.txt'))
+
+def load_to_tf_tests():
+	encoder = Encoder()
+	encoder.re_init(default_mod = 'BPSK_Modulator')
+	matrix = encoder.load_from_file('waveform_samples.txt')
+	encoder.numpy_array_to_tfdata(matrix)
+	#encoder.save_to_file(dataset)
 
 if __name__ == '__main__':
 	try:
-		main()
+		#string_to_save_tests()
+		load_to_tf_tests()
 	except KeyboardInterrupt:
 		print("KeyboardInterrupt")
 		exit()
