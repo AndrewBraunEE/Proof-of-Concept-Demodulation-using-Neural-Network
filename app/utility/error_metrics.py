1dafb1646d352bb65b6811e0c748709ecf1caf1a
1337eeab <andrewtb97@hotmail.com>
2019-03-07 14:58:16 -0800

    push

--- app/utility/error_metrics.py
+++ app/utility/error_metrics.py
@@ -1,23 +1,33 @@
 import sys
+import numpy as np
 class ErrorMetrics:
 	def __init__(self, demodulator):
 		self.demodulator = demodulator
 
 	def BER(self, data, output_data):
 		wrong_bits = 0
-		for index, element in enumerate(data):
+		for index, element in np.ndenumerate(data):
+			#print('element: ' + str(element))
+			#print('element2: ' + str(output_data))
 			if element != output_data[index]:
 				wrong_bits = wrong_bits + 1
 		ber = wrong_bits / len(data)
 		return ber
 
-	def NVE(self, data_BER_array, output_data_BER_array): #This is the normalized validation error
+	def NVE(self, data_BER_array, output_data_BER_array, waveform): #This is the normalized validation error
 		NVE = 0.0
+		print(data_BER_array, output_data_BER_array)
 		if len(data_BER_array) != len(output_data_BER_array):
 			sys.stderr.write("Wrong length")
-		for index, element in enumerate(data_BER_array):
-			ber = BER(data, output_data)
-			ber_map = self.demodulator.demodulate()
-			NVE = NVE + (ber/ber_map)
+		ber = self.BER(data_BER_array, output_data_BER_array)
+		observed_demod = self.demodulator.demodulate(waveform)
+		ber_map = 0.0
+		index = 1
+		for element in output_data_BER_array:
+			element1 = int(data_BER_array[index], 2)
+			if element1 != element:
+				ber_map += 1.0
+			index += 1
+		NVE = NVE + (ber/ber_map)
 		NVE = NVE / len(data_BER_array)
 		return NVE
